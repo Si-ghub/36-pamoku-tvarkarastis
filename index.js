174ad6b63a2f897c89730b48e4467535cd0cab59
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-07-22 22:10:02 +0300

    Lessons

---
+++ index.js
@@ -0,0 +1,7 @@
+// čia pateikti visus required/import metodus
+// susikurti pavyzdinius objektus iš class
+// parodyti, jog sukurti metodai tikrai veikia, t.y.
+// prieš tai suvesta informacija turi įtaką likusiems
+// kviečiamiems metodams
+
+const sheduleData = require('./Schedule');
